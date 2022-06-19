5803afcb4ed5cf45d7c3f6a3855b6deea3c639d3
Szymon Marcinkiewicz <szymon.a.marcinkiewicz@allegro.pl>
2022-06-19 17:59:11 +0200

    Initialized Gradle Kotlin project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.7.0"
+}
+
+group = "dev.marcinkiewicz"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
